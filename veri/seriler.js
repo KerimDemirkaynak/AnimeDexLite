b1dff646bccda57b1d9434fa856e63e666879680
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2025-12-03 22:14:07 +0300

    Update seriler.js

--- veri/seriler.js
+++ veri/seriler.js
@@ -3,12 +3,22 @@ const TUM_SERILER = [
     "id": "beyblade",
     "isim": "Beyblade",
     "poster": "https://i.ebayimg.com/images/g/psMAAOSwKFFnq2Ws/s-l400.jpg",
-    "bolumSayisi": "51"
+    "bolumSayisi": "51",
+    "sure": "24 Dk.",
+    "yil": "2001",
+    "tur": "Aksiyon, Spor",
+    "tip": "TV",
+    "ozet": "Takao ve arkadaşlarının efsanevi maceraları başlıyor. Kutsal canavarların gücüyle turnuvanın şampiyonu olabilecekler mi?"
   },
   {
     "id": "bakugan",
     "isim": "Bakugan",
     "poster": "https://m.media-amazon.com/images/I/612F8bfniwL._AC_UF894,1000_QL80_.jpg",
-    "bolumSayisi": "276"
+    "bolumSayisi": "52",
+    "sure": "22 Dk.",
+    "yil": "2007",
+    "tur": "Fantezi, Oyun",
+    "tip": "TV",
+    "ozet": "Dan ve arkadaşlarının kartlarla başlayan oyunu, başka bir boyuttaki savaşa dönüşüyor."
   }
 ];
